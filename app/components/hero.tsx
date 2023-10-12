997090af5ec486187720c9bf090167b1e73c280a
Nathan Wang <nathan.wang2@gmail.com>
2023-10-11 22:04:48 -0700

    Fixed the spacing. Not enough padding on the hero section

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -5,7 +5,7 @@ export const Hero = () => {
     return (
         <>
             <div className="flex bg-yellow-500 text-black border-black border-b">
-                <div className="flex basis-1/2 space-y-9 flex-col py-20 pl-16">
+                <div className="flex basis-1/2 space-y-9 flex-col py-20 pl-32">
                     <h2 className="text-8xl">Stay curious.</h2>
                     <h3 className="text-xl">Discover stories, thinking, and expertise from writers on any topic.</h3>
                     <div>
